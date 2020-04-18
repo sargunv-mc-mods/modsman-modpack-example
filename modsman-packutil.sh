3689f76ef46a5bc2d9d1f595f8782a7b6c6b3cde
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-18 15:11:32 -0700

    oops

--- modsman-packutil.sh
+++ modsman-packutil.sh
@@ -5,6 +5,7 @@ FILE_URL="https://github.com/sargunv/modsman/releases/download/0.32.0/modsman-pa
 
 if [ ! -f "$FILE_NAME" ]; then
     echo "Downloading '$FILE_NAME' from '$FILE_URL' ..."
+    curl $FILE_URL --output $FILE_NAME
 fi
 
 java -jar $FILE_NAME $@
