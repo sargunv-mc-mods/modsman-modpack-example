9918c71987d28a5f910f0d14ab41c8e164dea95f
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-17 17:58:27 -0700

    flatten

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-javac -Xlint:-options -source 8 -target 8 -d pack\.modsman\bin FirstRun.java
+javac -Xlint:-options -source 8 -target 8 -d .modsman\bin FirstRun.java
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-javac -Xlint:-options -source 8 -target 8 -d pack/.modsman/bin FirstRun.java
+javac -Xlint:-options -source 8 -target 8 -d .modsman/bin FirstRun.java
